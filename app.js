49bf82da0c2a90a718839a46f87371e654c1b691
alyssonw2 <blogdoalysson@gmail.com>
2023-05-10 10:56:40 -0300

    updatte

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
-let dispositivoSelecionado;
-
-async function listarDispositivos() {
-  try {
-    const dispositivos = await navigator.bluetooth.requestDevice({
-      acceptAllDevices: true
-    });
-    dispositivos.forEach((dispositivo) => {
-      const item = document.createElement('li');
-      item.textContent = dispositivo.name;
-      item.addEventListener('click', async () => {
-        dispositivoSelecionado = await navigator.bluetooth.requestDevice({
-          filters: [{ name: dispositivo.name }]
-        });
-        mostrarInfoDispositivo();
+navigator.bluetooth.requestDevice({
+  acceptAllDevices: true
+})
+.then(dispositivo => {
+  const item = document.createElement('li');
+  item.textContent = dispositivo.name;
+  item.addEventListener('click', () => {
+      dispositivo.gatt.connect()
+      .then(server => {
+          dispositivoSelecionado = dispositivo;
+          mostrarInfoDispositivo();
       });
-      document.getElementById('dispositivos').appendChild(item);
-    });
-  } catch (erro) {
-    console.error('Erro ao listar dispositivos Bluetooth:', erro);
-  }
-}
-
-function mostrarInfoDispositivo() {
-  document.getElementById('info-dispositivo').style.display = 'block';
-  document.getElementById('nome-dispositivo').textContent = dispositivoSelecionado.name;
-  document.getElementById('id-dispositivo').textContent = dispositivoSelecionado.id;
-  document.getElementById('gatt-servicos').textContent = dispositivoSelecionado.gatt.services.map((servico) => servico.uuid).join(', ');
-}
-
-listarDispositivos();
+  });
+  document.getElementById('dispositivos').appendChild(item);
+})
+.catch(erro => {
+  console.error('Erro ao listar dispositivos Bluetooth:', erro);
+});
